4118d18214500f889385e7c19a90384383a0724e
itzel <itzeljimenez.do@gmail.com>
2017-08-07 01:49:29 -0500

    version desktop

--- js/main.js
+++ js/main.js
@@ -1,26 +1,8 @@
-var sectionSobremi = $("#sobre-mi");
-var sectionHabilidades = $("#habilidades");
-var sectionProyectos = $("#proyectos");
-var sectionContacto = $("#contacto");
-var sectionInicio = $("#portada");
-
-var botonSobremi = $(".sobre-mi");
-var botonHabilidades = $(".habilidades");
-var botonProyectos = $(".proyectos");
-var botonContacto = $(".contacto");
-var botonInicio = $(".inicio");
-
  var cargarPagina = function(){
  	mostrarPortada1();
  	cambiarPortadas();
- 	    $('.modal').modal();
 
   $('#menu').click( function(){ $('.tap-target').tapTarget('open');})
-  botonSobremi.click(mostrarSecSobreMi);
-  botonHabilidades.click(mostrarSecHabilidades);
-  botonProyectos.click(mostrarSecProyectos);
-  botonContacto.click(mostrarSecContacto);
-  botonInicio.click(mostrarSecInicio);
 
  };
 
@@ -56,45 +38,6 @@ var cambiarPortadas = function(){
 	}, 9000);	
 }
 
-var mostrarSecSobreMi = function(){
-      sectionSobremi.removeClass('hide');
-      sectionHabilidades.addClass('hide');
-      sectionProyectos.addClass('hide');
-      sectionContacto.addClass('hide');
-      sectionInicio.addClass('hide');
-}
 
-var mostrarSecInicio = function(){
-      sectionSobremi.addClass('hide');
-      sectionHabilidades.addClass('hide');
-      sectionProyectos.addClass('hide');
-      sectionContacto.addClass('hide');
-      sectionInicio.removeClass('hide');
-}
-
-var mostrarSecHabilidades = function(){
-      sectionSobremi.addClass('hide');
-      sectionHabilidades.removeClass('hide');
-      sectionProyectos.addClass('hide');
-      sectionContacto.addClass('hide');
-      sectionInicio.addClass('hide');
-}
-
-var mostrarSecProyectos = function(){
-      $('body').addClass('black');
-      sectionSobremi.addClass('hide');
-      sectionHabilidades.addClass('hide');
-      sectionProyectos.removeClass('hide');
-      sectionContacto.addClass('hide');
-      sectionInicio.addClass('hide');
-}
-
-var mostrarSecContacto = function(){
-      sectionSobremi.addClass('hide');
-      sectionHabilidades.addClass('hide');
-      sectionProyectos.addClass('hide');
-      sectionContacto.removeClass('hide');
-      sectionInicio.addClass('hide');
-}
 
  $(document).ready(cargarPagina);
